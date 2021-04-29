0ef3baa88bcbf104cbf694f89f29615d49fba1d1
Henry WKK <henry1987@gmail.com>
2021-04-29 09:51:17 +0800

    Create Book.java

---
+++ src/main/java/com/example/springcacheapp/domain/Book.java
@@ -0,0 +1,16 @@
+package com.example.caching.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Book {
+    private String isbn;
+    private String title;
+
+    @Override
+    public String toString() {
+        return "Book{" + "isbn='" + isbn + '\'' + ", title='" + title + '\'' + '}';
+    }
+}
